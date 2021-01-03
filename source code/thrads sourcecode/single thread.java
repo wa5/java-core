de9e818507cd6a2212439256e9e38f9319da9d07
wa5 <47422872+wa5@users.noreply.github.com>
2021-01-03 08:42:30 +0530

    Add files via upload

---
+++ source code/thrads sourcecode/single thread.java
@@ -0,0 +1,25 @@
+class Sthreads extends Thread{  
+public void run(){  
+System.out.println("thread is running...");  
+
+//countdown
+/*int i = 60;
+while (i>0){
+ System.out.println("Remaining: "+i+" seconds");
+ try {
+   i--;
+   Thread.sleep(1000L);    // 1000L = 1000ms = 1 second
+  }
+  catch (InterruptedException e) {
+      //I don't think you need to do anything for your particular problem
+  }
+}*/
+
+
+
+}  
+public static void main(String args[]){  
+Sthreads  t1=new Sthreads();  
+t1.start();  
+ }  
+}
